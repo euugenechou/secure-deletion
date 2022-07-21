9b5aad7139a1df6fe73a06a1cd4ae7a532916e8d
Ryan Williams <ryan.rpwilliams@gmail.com>
2022-07-21 08:19:43 -0400

    Parameterized a bit

--- fs_benchmarks/run_bench.sh
+++ fs_benchmarks/run_bench.sh
@@ -1,11 +1,46 @@
 #!/usr/bin/env bash
 
-# Run Bonnie++ with 512 files for creation/deletion.
+usage() {
+  printf "Usage: $0 -n [num] -d [dirs]\n\n";
+  printf "[num] : Number of files to create/delete\n"
+  printf "[dirs] : Number of subdirectories to split files across (default: 0)\n\n"
+  exit 1
+}
+
+if [ "$#" -ne 4 ]; then
+  usage
+fi
+
+while [ "$1" != "" ]; do
+  case $1 in
+      -h|--help)
+	usage
+	;;
+      -n|--num) 
+        FILES=$2
+        shift
+        ;;  
+      -d|--dirs)
+        DIRS=$2
+        shift
+        ;;
+      *)  
+        break
+        ;;  
+  esac
+  shift
+done
+
+printf "[+] Running Bonnie++ with $FILES files across $DIRS sub-directories...\n"
+
+# Run Bonnie++ with 512*1024 files for creation/deletion.
 # Set -r to installed RAM. Bonnie++ uses RAM*2 for file size.
 # User root:root for running in QEMU.
 #time bonnie++ -d . -r 16000 -n 512 -u root:root > perf.txt
-time bonnie++ -d . -r 16000 -n 512 > perf.txt
+time bonnie++ -d . -r 16000 -n $FILES:$DIRS > perf.txt
 
 # Generate HTML report.
 tail -1 perf.txt | bon_csv2html > perf.html
 
+# View report.
+firefox perf.html &
